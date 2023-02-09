d04a8cbfe8070712413c40f9e71c908a3d9526ba
Joao Gabriel Vargas <joao.gaiares@gmail.com>
2023-02-09 09:36:31 -0300

    feat: implementa requisicao da API e retorna os cards dos repositorios

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
-import image from '../assets/image.jpg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import Card from '../components/Card';
 import styles from './Profile.module.css';
 
 function Profile() {
+  const [ details, setDetails ] = useState({});
+  const [repos, setRepos] = useState();
+  const { user } = useParams();
+  
+
+  useEffect(()=> {
+    const getData = async () => {
+      await axios.get(`https://api.github.com/users/${user}`)
+      .then( async (response) => {setDetails(response.data)
+        await axios.get(`https://api.github.com/users/${user}/repos`)
+        .then((response) => setRepos(response.data) )
+      })
+    };
+    getData();
+  }, []);
   return (
     <>
       <div className={styles.details}>
-        <img src={image} alt="profile" />
+        <img src={details.avatar_url} alt="profile" />
         <div className={styles.user}>
           <div className={styles.details_user}>
             <div className={styles.followers_details}>
-              <p> 267</p>
+              <p> {details.followers}</p>
               <p> Seguidores </p>
             </div>
             <div className={styles.followers_details}>
-              <p> 267</p>
-              <p> Seguidores </p>
+              <p> {details.following}</p>
+              <p> Seguindo </p>
             </div>
             <div className={styles.followers_details}>
-              <p> 267</p>
-              <p> Seguidores </p>
+              <p> {details.public_repos}</p>
+              <p> Repositórios </p>
             </div>
           </div>
           <div className={styles.username_details}>
-            <h3> username</h3>
-            <p> description </p>
+            <h3> {details.login}</h3>
+            <p> {details.bio} </p>
           </div>
         </div>
       </div>
       <div>
-        <Card />
+        {repos && repos.map((repo)=> {
+          return (
+            <Card 
+              username={ repo.full_name} 
+              description ={repo.description} 
+              image ={details.avatar_url} 
+              route={repo.html_url}
+            />
+          )
+        })}
       </div>
     </>
   );
